5320bab2c7a58a6a545173d837068d7a3bd31890
Stepan Mamonov <stepan.mamonov.1406@gmail.com>
2024-01-15 19:35:02 +0800

    подключил базу данных  и приложение "users"

--- sncore/settings.py
+++ sncore/settings.py
@@ -19,6 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'modules.users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -53,8 +55,12 @@ WSGI_APPLICATION = 'sncore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('ENGINE'),
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('DBUSER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
