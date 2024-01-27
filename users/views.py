08267606be905da4fe6cb88a5aeaa38ebc01f721
Stepan Mamonov <stepan.mamonov.1406@gmail.com>
2024-01-27 20:57:35 +0800

    добалено представление входа в систему

--- users/views.py
+++ users/views.py
@@ -1,1 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib .auth import authenticate, login
+
+from .forms import LoginForm
+
+
+def user_login(request):
+    template_name = 'users/login.html'
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cleandata = form.cleaned_data
+            user = authenticate(request,
+                                username=cleandata['username'],
+                                password=cleandata['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Вы успешно вошли')
+                else:
+                    return HttpResponse('Доступ запрещен')
+            else:
+                return HttpResponse('Ошибка входа')
+        else:
+            form = LoginForm()
+        return render(request, template_name, {'form': form})
